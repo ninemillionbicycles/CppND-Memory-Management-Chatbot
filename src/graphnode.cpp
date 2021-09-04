bf258a198f23ffe3c2f4059f45f6b9a1d8db8f93
ninemillionbicycles <hannah.theuer@gmail.com>
2021-09-04 03:55:14 +0200

    Use move semantics for child edges

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -15,7 +15,7 @@ void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
     _parentEdges.emplace_back(edge);
 }
 
-void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)
+void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> &&edge)
 {
     _childEdges.emplace_back(std::move(edge));
 }
