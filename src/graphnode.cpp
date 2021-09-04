304b65b4e18229cc2f4be8c9e431e366ceb4b273
ninemillionbicycles <hannah.theuer@gmail.com>
2021-09-04 04:29:01 +0200

    Use move semantics for ChatBot

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -15,20 +15,20 @@ void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
     _parentEdges.emplace_back(edge);
 }
 
-void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> &&edge)
+void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)
 {
     _childEdges.emplace_back(std::move(edge));
 }
 
 void GraphNode::MoveChatbotHere(ChatBot chatbot)
 {
-    _chatBot = chatbot;
+    _chatBot = std::move(chatbot);
     _chatBot.SetCurrentNode(this);
 }
 
 void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
 {
-    newNode->MoveChatbotHere(_chatBot);
+    newNode->MoveChatbotHere(std::move(_chatBot));
 }
 
 GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
